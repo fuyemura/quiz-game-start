660d224a6ea29b2104ede823b006ce540c8a903c
Flávio Uyemura <fuyemura@gmail.com>
2022-11-09 23:12:53 -0300

    Segunda versão

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ for question in question_data:
     question_text = question['text']
     question_answer = question['answer']
     new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank)
 
 
